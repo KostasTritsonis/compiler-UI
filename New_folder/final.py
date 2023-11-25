a96bb2aca5f387bbe65b03445e3fba3fe5da8e2b
KostasTritsonis <ktritsonis@hotmail.com>
2023-11-25 10:21:37 +0200

    251123

--- New_folder/final.py
+++ New_folder/final.py
@@ -1,4 +1,4 @@
-import sys,subprocess
+import sys
 instructions,table = [],[]
 results,commands,temp = {},{},{}
 
@@ -42,11 +42,10 @@ def readTable(number):
     
     
 def block():
-    global instructions,lines,commands,numpass
-    numpass = -1
+    global instructions,lines,commands
+    
     i=0
     while  i < len(instructions):
-        
         if instructions[i][0] == 'halt':
             break  
         
@@ -54,112 +53,84 @@ def block():
             lines -=1
             if lines == 0:
                 break
-        if numpass == -1 or i == int(numpass)-1:
-            numpass = funCommands(instructions[i])
-          
-        i+=1  
-    code.write('\n{a}()'.format(a=list(temp)[-1]))
-    code.close()
+       
+        j = funCommands(instructions[i])
+        if j!=-1:
+            i=int(j)
+        else:
+            i+=1  
     return
  
  
  
-name =''
+name= ''
 counter,totalSpace=0,0
-code = open('finalCode.py','w+')
-listOfCode,par = [],[]   
+par,code =[],[]
+listofCode = {}   
 
 def funCommands(i):
-    global counter,name,code,par,totalSpace
-    
+    global counter,name,par,totalSpace,code,return1
+    code.append(i)
     if i[0] == 'begin_block':
         totalSpace = readTable(counter)
-        code.write(totalSpace*'\t'+'def {func}():\n'.format(func=i[1]))
+       
         name = i[1]
-        totalSpace+=1   
-        
+        totalSpace+=1       
     elif i[0] == ':=':
-        code.write(totalSpace*'\t'+'{c} = {a}\n'.format(a=i[-1],c=i[1]))
-        
         if i[1] in temp[name] and temp[name][i[1]]!=0:
          temp[name][i[-1]] = temp[name][i[1]]
         else:
           temp[name][i[-1]] = checkString(i[1])  
             
     elif i[0] == '+':
-        code.write(totalSpace*'\t'+'{c} = {a} + {b}\n'.format(a=i[1],b=i[2],c=i[3]))
         temp[name][i[-1]]  = temp[name][i[1]] + temp[name][i[2]]
         
     elif i[0] == '/':
-        code.write(totalSpace*'\t'+'{c} = {a} / {b}\n'.format(a=i[1],b=i[2],c=i[3]))
         temp[name][i[-1]]  = temp[name][i[1]] / temp[name][i[2]]
         
     elif i[0] == '-':
-        code.write(totalSpace*'\t'+'{c} = {a} - {b}\n'.format(a=i[1],b=i[2],c=i[3]))
         temp[name][i[-1]]  = temp[name][i[1]] - temp[name][i[2]]
         
     elif i[0] == '*':
-        code.write(totalSpace*'\t'+'{c} = {a} * {b}\n'.format(a=i[1],b=i[2],c=i[3]))
         temp[name][i[-1]]  = temp[name][i[1]] * temp[name][i[2]]
         
     elif i[0] == 'out':
-        code.write(totalSpace*'\t'+'print({c})\n'.format(c=i[1]))
+       print(temp[name][i[1]])
         
     elif i[0] == 'inp':
         print("Give input for {value}:".format(value=i[1]))
         temp1 =  sys.stdin.readline().strip()
         temp[name][i[1]]  = checkString(temp1)
-        code.write(totalSpace*'\t'+'{a} = {b}\n'.format(a=i[1],b=temp1))
         
     elif i[0] == 'retv':
-        code.write(totalSpace*'\t'+'return {d}\n\n'.format(d=i[1]))
+        return1 =  temp[name][i[1]]
         
     elif i[0] == 'end_block':
+        del code[0]
+        del code[-1]
+        listofCode[name] = code
         counter+=1
-        listOfCode.append(code)
+        code=[]
         
     elif i[0] == '=' or  i[0] == '<' or  i[0] == '>' or  i[0] == '!=' :
-        if i[0] == '=':
-            code.write(totalSpace*'\t'+'if {a} {b} {c}:\n'.format(a=i[1],b='==',c=i[2]))
-        else:
-            code.write(totalSpace*'\t'+'if {a} {b} {c}:\n'.format(a=i[1],b=i[0],c=i[2]))
-        totalSpace+=1
-        return i[3]
+        return i[-1]
         
     elif i[0] == 'par':
-        if i[2] == 'RET':
-            code.write(totalSpace*'\t'+'{a} = '.format(a=i[1]))
-        else:
-            par.append(i[1])
             
-    elif i[0] == 'call':
-        c = ''
-        for j in par:
-            c +='{j},'.format(j=j)
-        c = c.strip(',')
-        code.write('{a}({b})\n'.format(a=i[1],b=c))
-        
-        new_line = 'def {a}({b}):\n'.format(a=i[1],b=c)
-        
-        code.seek(0)
-        file = code.readlines()
-        temporary = 'def {a}():\n'.format(a=i[1])
-        for line in file:            
-            if temporary in line:
-                file[file.index(line)] = new_line
-        
-        code.seek(0)
-        code.writelines(file) 
-        code.seek(code.tell()) 
-        par = []
+        if i[2] == 'RET':
+            temp[name][i[1]] = return1
+        else:
+            par.append(i[1])  
     
-        
     elif i[0] == 'jump':
-        totalSpace-=1
-        code.write(totalSpace*'\t'+'else:\n')
-        totalSpace+=1
-        
+        return i[-1]   
     
+    elif i[0] == 'call':
+        for i in listofCode[i[1]]:
+            funCommands(i)
+             
+   
+          
     return -1
             
     
@@ -196,7 +167,6 @@ if __name__ == '__main__':
     readIntermidiate()
     checkBreakpoint()
     block()
-    subprocess.run(['python', 'finalCode.py'])
     if breakpoint != 'None':
         printTable()
     print(temp)
